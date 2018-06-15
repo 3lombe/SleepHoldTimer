64f28977df7d0f8f1e1f2a4239075744b353aed8
Nsonsa <elombekisala@gmail.com>
2018-06-14 22:41:54 -0400

    Initial Commit

---
+++ SleepTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SleepTimer
+//
+//  Created by Nsonsa Elombe Kisala Jr. on 6/14/18.
+//  Copyright © 2018 Nsonsa Kisala Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
