955a0e1a0043aea14e1b3ddbc8aff8055085dd61
Nsonsa <elombekisala@gmail.com>
2018-06-15 16:06:43 -0400

    Created variables for timer/stopwatch. incorporated variables into updateStopwatch method. When Hold button is pressed, updateStopwatch method is called and updates the stopwatchLabel. When button is no longer pressed (press state is ended), the timer stops running (is paused).

--- SleepTimer/ViewController.swift
+++ SleepTimer/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var sleepBtn: UIButton!
     @IBOutlet weak var nightSwitch: UISwitch!
+    @IBOutlet weak var stopwatchLabel: UILabel!
+    
+    
+    var timer = Timer()
+    var minutes : Int = 0
+    var seconds : Int = 0
+    var fractions : Int = 0
+    
+    let startStopwatch : Bool = true
+    var stopwatchString = ""
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +35,38 @@ class ViewController: UIViewController {
     @objc func LongPressAction(sender: UIGestureRecognizer) {
         if sender.state == .began {
             print("Pressed")
+            
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateStopwatch), userInfo: nil, repeats: true)
+            
+            
         } else if sender.state == .ended {
             print("Ended")
+            
+            timer.invalidate()
+            
         }
     }
     
     @objc func NightSwitchOn(sender: UISwitch) {
         print("switch is on")
+    }
+    
+    @objc func updateStopwatch() {
+        fractions += 1
+        if fractions == 100 {
+            seconds += 1
+            fractions = 0
+        }
+        
+        if seconds == 60 {
+            minutes += 1
+            seconds = 0
+        }
+        let fractionsString = fractions > 9 ? "\(fractions)" : "0\(fractions)"
+        let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+        let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+        let stopwatchString = "\(minutesString):\(secondsString):\(fractionsString)"
+        stopwatchLabel.text = String(stopwatchString)
     }
 
 
