791672ce2aa5837a7df7248b2dbf35a6add018b4
Nsonsa <elombekisala@gmail.com>
2018-06-16 08:58:34 -0400

    Created SettingsViewController and imported AnalogClock to the view. Created a new UIview within settings view and assigned it to the AnalogClockView.

---
+++ SleepTimer/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  SleepTimer
+//
+//  Created by Nsonsa Elombe Kisala Jr. on 6/16/18.
+//  Copyright © 2018 Nsonsa Kisala Jr. All rights reserved.
+//
+
+import UIKit
+import AnalogClock
+
+class SettingsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
